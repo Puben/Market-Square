a8d5daaccb5d872e38e64fa69db40917701e72ab
Webber89 <andreasweber1989@hotmail.com>
2015-03-11 03:08:33 +0100

    Search area
    Trying to implement an area that will be searched. Not yet fully
    implemented, circle is there

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,6 +10,41 @@ var locations = {
 var center = locations["Copenhagen"];
 var radiusInKm = 1;
 
+// Create a new GeoFire instance, pulling data from the public transit data
+
+var rootRef = new Firebase('https://marketsquare.firebaseio.com/');
+var adsRef = rootRef.child("ads");
+var geoFire = new GeoFire(rootRef.child("_geofire"));
+
+var adsInQuery = {};
+
+var geoQuery = geoFire.query({
+  center: center,
+  radius: radiusInKm
+});
+
+geoQuery.on("key_entered", function(adId, adLocation) {
+    // Specify that the vehicle has entered this query
+    console.log("you have entered the gateway to hell..");
+    adId = adId.split(":")[1];
+    adsInQuery[adId] = true;
+
+  // Look up the vehicle's data in the Transit Open Data Set
+    adsRef.child().once("value", function(dataSnapshot) {
+    // Get the vehicle data from the Open Data Set
+	ad = dataSnapshot.val();
+
+    // If the vehicle has not already exited this query in the time it took to look up its data in the Open Data
+    // Set, add it to the map
+    if (ad !== null && adsInQuery[adId] === true) {
+      // Add the vehicle to the list of vehicles in the query
+      adsInQuery[adId] = ad;
+
+      // Create a new marker for the vehicle
+      vehicle.marker = createVehicleMarker(vehicle, getVehicleColor(vehicle));
+    }
+  });
+});
 
 /*****************/
 /*  GOOGLE MAPS  */
@@ -21,11 +56,32 @@ function initializeMap() {
     var loc = new google.maps.LatLng(center[0], center[1]);
 
     // Create the Google Map
-        map = new google.maps.Map(document.getElementById("map-canvas"), {
+    map = new google.maps.Map(document.getElementById("map-canvas"), {
         center: loc,
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+
+    var circle = new google.maps.Circle({
+	strokeColor: "#6D3099",
+	strokeOpacity: 0.7,
+	strokeWeight: 1,
+	fillColor: "#B650FF",
+	fillOpacity: 0.35,
+	map: map,
+	center: loc,
+	radius: ((radiusInKm) * 1000),
+	draggable: true
+    });
+
+    var updateCriteria = _.debounce(function() {
+	var latLng = circle.getCenter();
+	geoQuery.updateCriteria({
+	    center: [latLng.lat(), latLng.lng()],
+	    radius: radiusInKm
+	});
+    }, 10);
+    google.maps.event.addListener(circle, "drag", updateCriteria);
 };
 
 function drawMarker(latitude, longitude){
@@ -35,12 +91,12 @@ function drawMarker(latitude, longitude){
     });
 }
 
-var rootRef = new Firebase('https://marketsquare.firebaseio.com/');
-var adsRef = rootRef.child("ads");
-
 adsRef.on('child_added', function(snapshot) {
     drawMarker(snapshot.val().latitude, snapshot.val().longitude);
-    });
+});
+adsRef.on('child_removed', function(snapshot) {
+    
+});
 
 /*      // Create a draggable circle centered on the map
   var circle = new google.maps.Circle({
